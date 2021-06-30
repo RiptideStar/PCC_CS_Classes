f2cc1c41e532a6ca039ff166172b8583b2da7835
Kyle Z <kyle1001001@gmail.com>
2021-06-29 23:09:45 -0700

    cis 275 first sql

---
+++ cis275/MyStore2.sql
@@ -0,0 +1,21 @@
+CREATE TABLE Customer (
+    CustomerId int,
+    FirstName varchar(64),
+    LastName varchar(64),
+    Email varchar(64),
+    Address varchar(64),
+    City varchar(64),
+    StateProvence varchar(64),
+    Country varchar(64)
+);
+
+INSERT INTO Customer (CustomerId, FirstName, LastName, Email, Address, City, StateProvence, Country)
+VALUES (1, Tyson, Ngo, tenz@gmail.com, 45 Ontario Street, Ottawa, Ontario, Canada);
+INSERT INTO Customer (CustomerId, FirstName, LastName, Email, Address, City, StateProvence, Country)
+VALUES (2, Tom, Yala, tyala@gmail.com, 56 Toronto Street, Toronto, Ontario, Canada);
+INSERT INTO Customer (CustomerId, FirstName, LastName, Email, Address, City, StateProvence, Country)
+VALUES (3, Tom, Yala, tyala@gmail.com, 56 Toronto Street, Toronto, Ontario, Canada);
+INSERT INTO Customer (CustomerId, FirstName, LastName, Email, Address, City, StateProvence, Country)
+VALUES (4, Mariano, Arruda, tenz@gmail.com, 45 Ontario Street, Ottawa, Ontario, Canada);
+INSERT INTO Customer (CustomerId, FirstName, LastName, Email, Address, City, StateProvence, Country)
+VALUES (5, Matthew, Yarckun, clover@gmail.com, MLK Street, Portland, Oregon, USA);
